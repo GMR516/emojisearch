17efc253e62593324c188b3f442e8aeb28e56bd5
GMR516 <GMR516@users.noreply.github.com>
2020-06-29 00:20:00 -0700

    First build

---
+++ precache-manifest.ce1707164d9d0668b12bfef2be390fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765a5f29f7c8e7a3eb0bc31041347ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6dd05768d547b689da3",
+    "url": "/static/css/main.9432b794.chunk.css"
+  },
+  {
+    "revision": "72bcd6fc2121b97af693",
+    "url": "/static/js/2.3e4940bc.chunk.js"
+  },
+  {
+    "revision": "fa9f8d86bca7ba94baa2c0fa4755314d",
+    "url": "/static/js/2.3e4940bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6dd05768d547b689da3",
+    "url": "/static/js/main.adb1c262.chunk.js"
+  },
+  {
+    "revision": "1dc3b027e697dd836b3e",
+    "url": "/static/js/runtime-main.5b7c67a2.js"
+  }
+]);
